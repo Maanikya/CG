95f8d752f6d8a6c35597cc4bb6a5aa1782ee8807
Maanikya <maanikya.acharya@gmail.com>
2022-05-29 13:10:56 +0530

    Added Intro Slide

--- miniProject.c
+++ miniProject.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include "Intro.h"
+#include "Buildings.h"
 
 void WindowInit();
-void Scene();
-void output(char *s);
 
 int main(int argc, char *argv[]) 
 {
@@ -21,33 +21,15 @@ int main(int argc, char *argv[])
 
 void WindowInit()
 {
-	glutInitWindowSize(1280, 720);
-	glutInitWindowPosition(300, 500);
+	glutInitWindowSize(1920, 1080);
+	glutInitWindowPosition(0, 0);
 	glutCreateWindow("Mini Project");
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glClearColor(1.0, 1.0,	1.0, 0.0);
+	glClearColor(0.0, 0.0, 0.0, 0.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glOrtho(0.0, 100.0, 0.0, 100.0, 0.0, 100.0);
-	glutDisplayFunc(Scene);
+	glutDisplayFunc(Intro);
 	glutMainLoop();
-}
-
-void Scene()
-{
-	glClear(GL_COLOR_BUFFER_BIT);
-	glPushMatrix();
-	glColor3f(1.0, 0.0, 0.0);
-	glRasterPos3f(20.0, 60.0, 0.0);
-	glScalef(0.0, 0.0, 0.0);
-	output("Mangalore Institute of Technology & Engineering");
-	glPopMatrix();
-	glFlush();
-}
-
-void output(char *s)
-{
-	for(int k=0; k<strlen(s); k++)
-		glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[k]);
 }
