e1ef8333b8a7e96773e1cf1b17687473cdb6d4a9
Maanikya <maanikya.acharya@gmail.com>
2022-07-03 15:00:13 +0530

    Updated Night Sky

--- miniProject.c
+++ miniProject.c
@@ -3,7 +3,7 @@
 #include <GL/glut.h>
 
 float sunx=0, suny=50, sunColor=0.0, bk1Color1=0, bk1Color2=0, bk1Color3=0;
-float moonx=0, moony=50, bk2Color1=0, bk2Color2=0, bk2Color3=0, ground=0;
+float moonx=0, moony=50, bk2Color1=0, bk2Color2=0, bk2Color3=0, windows=0.0, ground=0, glow1=0, glow2=1;
 float shd=0.1, bk1=0, bk2=0;
 int slide;
 
@@ -13,6 +13,9 @@ void Display1();
 void Display2();
 //void Display3();
 void EffielTower();
+void Stars();
+void Stars1();
+void Stars2();
 void Timer1();
 void Timer2();
 //void Timer3();
@@ -44,6 +47,7 @@ void WindowInit()
 	glOrtho(0.0, 100.0, 0.0, 100.0, -10.0, 10.0);
 	glutDisplayFunc(Display);
 	glutKeyboardFunc(Keyboard);
+	//glutIdleFunc(Stars);
 	Timer1(0);
 	Timer2(0);
 	glutMainLoop();
@@ -83,19 +87,26 @@ void Keyboard(unsigned char key, int x, int y) {
 	
 	else if(key == '2') {
 		
-		moonx=0, moony=50, bk2Color1=0.3, bk2Color2=0.2, bk2Color3=0.1, shd=0.1, bk1=0, bk2=0, ground=0;
+		moonx=0, moony=50, bk2Color1=0.3, bk2Color2=0.2, bk2Color3=0.1, shd=0.1, bk1=0, bk2=0, windows=0.0, ground=0, glow1=0, glow2=1;
 		slide = 2;
 		//Timer2(0);
 		//glutPostRedisplay(); 
 	}
 	
-	/*else if(key == 'c') {
+	/*else if(key == '3') {
 		slide = 3;
 		glutPostRedisplay();
 	}*/
 	
 	glutPostRedisplay();
 }
+
+/*void Stars() {
+	
+	
+		
+	glutPostRedisplay();
+}*/
 
 void Timer1(int v) {
 	
@@ -178,9 +189,26 @@ void Timer2(int v) {
 	
 	if(ground >= 80)
 		ground=ground;
-	
 	else
-		ground += 1; 
+		ground += 1;
+		
+		
+	if(windows >= 1)
+		windows = windows;
+	else
+		windows += 0.01;
+		 
+		 
+	if(glow1 >= 1)
+		glow1 = 0;
+	else
+		glow1 += 0.015;
+		
+		
+	if(glow2 <= 1)
+		glow2 += 0.015;
+	else
+		glow2 = 0;
 	
 	glutTimerFunc(40, Timer2, 0); 	
 	glutPostRedisplay();
@@ -1112,7 +1140,7 @@ void Display2() {
 	
 	for(int y=0; y<12; y+=2){
 		glBegin(GL_POLYGON);						//Building 1 Windows
-		glColor3f(0.161, 0.188, 0.1);	
+		glColor3f(0.161+windows*0.7, 0.188+windows*0.7, 0.1-windows*0.1);	
 		glVertex2f(2.4, 42.0+y);
 		glVertex2f(2.4, 42.5+y);
 		glVertex2f(4.5, 42.5+y);
@@ -1148,7 +1176,7 @@ void Display2() {
 	
 	for(float y=0; y<15; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 2 Windows Left
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.7, 0.188+windows*0.7, 0.1-windows*0.1);	
 		glVertex2f(11.7, 38+y);
 		glVertex2f(11.7, 38.2+y);
 		glVertex2f(12.2, 38.2+y);
@@ -1166,7 +1194,7 @@ void Display2() {
 	
 	for(float y=0; y<15; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 2 Windows Right
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3ub(52+windows*157, 81+windows*128, 108-windows*108);	
 		glVertex2f(13.4, 38+y);
 		glVertex2f(13.4, 38.2+y);
 		glVertex2f(14.4, 38.2+y);
@@ -1200,7 +1228,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 3 Windows Right
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.8, 0.188+windows*0.8, 0.1-windows*0.1);	
 		glVertex2f(20.6, 34.4+y);
 		glVertex2f(20.6, 34.6+y);
 		glVertex2f(21.4, 34.6+y);
@@ -1234,7 +1262,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 4 Windows Right
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.8, 0.188+windows*0.8, 0.2-windows*0.2);	
 		glVertex2f(24.6, 34.4+y);
 		glVertex2f(24.6, 34.6+y);
 		glVertex2f(25.4, 34.6+y);
@@ -1252,7 +1280,8 @@ void Display2() {
 	
 	for(float y=0; y<2; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 5 Windows
-		glColor3ub(51, 75, 86);	
+		//glColor3ub(51, 75, 86);
+		glColor3ub(51+windows*128, 75+windows*104, 86-windows*86);	
 		glVertex2f(28.2+y, 38.4);
 		glVertex2f(28.2+y, 44.6);
 		glVertex2f(28.3+y, 44.6);
@@ -1270,7 +1299,8 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 6 Face 1 Windows
-		glColor3ub(48, 70, 79);	
+		//glColor3ub(48, 70, 79);	
+		glColor3ub(48+windows*181, 70+windows*159, 79-windows*79);
 		glVertex3f(29.7, 38.6+y, -4);
 		glVertex3f(29.7, 38.8+y, -4);
 		glVertex3f(32.3, 38.8+y, -4);
@@ -1295,7 +1325,8 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 6 Face 2 Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(32.6, 38.6+y);
 		glVertex2f(32.6, 38.8+y);
 		glVertex2f(33.4, 38.8+y);
@@ -1313,7 +1344,8 @@ void Display2() {
 	
 	for(float y=0; y<2; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 7 Windows
-		glColor3ub(51, 75, 86);	
+		//glColor3ub(51, 75, 86);
+		glColor3ub(51+windows*128, 75+windows*104, 86-windows*86);	
 		glVertex2f(33.6+y, 38.4);
 		glVertex2f(33.6+y, 44.6);
 		glVertex2f(33.7+y, 44.6);
@@ -1331,7 +1363,8 @@ void Display2() {
 	
 	for(float y=0; y<6; y+=0.8) {
 		glBegin(GL_POLYGON);						//Building 8 Windows
-		glColor3ub(51, 66, 75);	
+		//glColor3ub(51, 66, 75);
+		glColor3ub(51+windows*178, 66+windows*163, 75-windows*75);
 		glVertex2f(35.2, 38.4+y);
 		glVertex2f(35.2, 38.6+y);
 		glVertex2f(36.4, 38.6+y);
@@ -1349,7 +1382,8 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 9 Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(32.6, 38.6+y);
 		glVertex2f(32.6, 38.8+y);
 		glVertex2f(33.4, 38.8+y);
@@ -1367,7 +1401,8 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 10 Face 1 Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(36.8, 38.6+y);
 		glVertex2f(36.8, 38.8+y);
 		glVertex2f(37.4, 38.8+y);
@@ -1425,7 +1460,8 @@ void Display2() {
 	
 	for(float y=2; y<11; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 12 Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(41.2, 38.6+y);
 		glVertex2f(41.2, 38.8+y);
 		glVertex2f(43.8, 38.8+y);
@@ -1510,7 +1546,8 @@ void Display2() {
 	
 	for(float y=4; y<14; y+=0.7) {
 		glBegin(GL_POLYGON);						//Building 13 Windows
-		glColor3ub(49, 69, 78);	
+		//glColor3ub(49, 69, 78);
+		glColor3ub(49+windows*180, 69+windows*160, 78-windows*78);	
 		glVertex2f(48.5, 35.6+y);
 		glVertex2f(48.5, 35.8+y);
 		glVertex2f(50.4, 36.4+y);
@@ -1546,7 +1583,8 @@ void Display2() {
 	
 	for(float y=2; y<10; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 14 Left Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(54.2, 25.6+y);
 		glVertex2f(54.2, 25.8+y);
 		glVertex2f(54.8, 25.8+y);
@@ -1556,7 +1594,8 @@ void Display2() {
 	
 	for(float y=7; y<12; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 14 Right Windows
-		glColor3ub(76, 89, 93);	
+		//glColor3ub(76, 89, 93);
+		glColor3ub(76+windows*153, 89+windows*140, 93-windows*93);	
 		glVertex2f(56.2, 25.6+y);
 		glVertex2f(56.2, 25.8+y);
 		glVertex2f(56.8, 25.8+y);
@@ -1595,7 +1634,8 @@ void Display2() {
 	
 	for(float y=3; y<9; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 15 Windows Left
-		glColor3ub(52, 81, 108);	
+		//glColor3ub(52, 81, 108);
+		glColor3ub(52+windows*137, 81+windows*108, 108-windows*108);	
 		glVertex2f(59.3, 35.6+y);
 		glVertex2f(59.3, 35.8+y);
 		glVertex2f(59.9, 35.8+y);
@@ -1605,7 +1645,8 @@ void Display2() {
 	
 	for(float y=3; y<9; y+=0.5) {
 		glBegin(GL_POLYGON);						//Building 15 Windows Left
-		glColor3ub(52, 81, 108);	
+		//glColor3ub(52, 81, 108);
+		glColor3ub(52+windows*157, 81+windows*128, 108-windows*108);	
 		glVertex2f(60.6, 35.6+y);
 		glVertex2f(60.6, 35.8+y);
 		glVertex2f(61.2, 35.8+y);
@@ -1625,7 +1666,8 @@ void Display2() {
 	
 	for(int y=0; y<12; y+=2){
 		glBegin(GL_POLYGON);						//Building 16 Windows
-		glColor3f(0.161, 0.188, 0.1);	
+		//glColor3f(0.161, 0.188, 0.1);
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.1-windows*0.1);	
 		glVertex2f(72.4, 41.0+y);
 		glVertex2f(72.4, 41.5+y);
 		glVertex2f(74.5, 41.5+y);
@@ -1659,7 +1701,8 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.2) {
 		glBegin(GL_POLYGON);						//Building 17 Windows Right
-		glColor3f(0.161, 0.188, 0.2);	
+		//glColor3f(0.161, 0.188, 0.2);
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.2-windows*0.2);	
 		glVertex2f(78.6, 34.4+y);
 		glVertex2f(78.6, 34.6+y);
 		glVertex2f(79.4, 34.6+y);
@@ -1693,7 +1736,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.4) {
 		glBegin(GL_POLYGON);						//Building 18 Windows 1
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.2-windows*0.2);	
 		glVertex2f(82.5, 34.8+y);
 		glVertex2f(82.5, 35.6+y);
 		glVertex2f(83.0, 35.6+y);
@@ -1703,7 +1746,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.4) {
 		glBegin(GL_POLYGON);						//Building 18 Windows 2
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.2-windows*0.2);	
 		glVertex2f(83.7, 34.8+y);
 		glVertex2f(83.7, 35.6+y);
 		glVertex2f(84.2, 35.6+y);
@@ -1720,7 +1763,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.4) {
 		glBegin(GL_POLYGON);						//Building 18 Windows 3
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.2-windows*0.2);	
 		glVertex2f(85.9, 34.8+y);
 		glVertex2f(85.9, 35.6+y);
 		glVertex2f(86.4, 35.6+y);
@@ -1730,7 +1773,7 @@ void Display2() {
 	
 	for(float y=0; y<13; y+=1.4) {
 		glBegin(GL_POLYGON);						//Building 18 Windows 4
-		glColor3f(0.161, 0.188, 0.2);	
+		glColor3f(0.161+windows*0.839, 0.188+windows*0.812, 0.2-windows*0.2);	
 		glVertex2f(87, 34.8+y);
 		glVertex2f(87, 35.6+y);
 		glVertex2f(87.5, 35.6+y);
@@ -1748,7 +1791,8 @@ void Display2() {
 	
 	for(float y=0; y<8; y+=1.4) {
 		glBegin(GL_POLYGON);						//Building 19 Windows
-		glColor3f(0.161, 0.188, 0.2);	
+		//glColor3f(0.161+windows*0.9, 0.188+windows*0.9, 0.2-windows*0.2);
+		glColor3ub(52+windows*157, 81+windows*128, 108-windows*108);
 		glVertex2f(90.4, 25.9+y);
 		glVertex2f(90.4, 26.6+y);
 		glVertex2f(94.5, 25.8+y);
@@ -1960,10 +2004,15 @@ void Display2() {
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	glPushMatrix();
-	glTranslatef(moonx, moony, -9.0);
+	glTranslatef(moonx, moony, -8.0);
 	glScalef(0.6, 1.0, 1.0);
-	glutSolidSphere(2.5, 40, 40);
+	glutSolidSphere(2.8, 40, 40);
 	glPopMatrix();
+	
+	
+	
+	Stars1();
+	Stars2();
 	
 	glutSwapBuffers;
 	glFlush();
@@ -2051,11 +2100,11 @@ void EffielTower(){
 	}
 	
 	glBegin(GL_POLYGON);
-	glColor3ub(225, 228, 219);
-	glVertex3f(66.8, 43, -3);
-	glVertex3f(66.6, 44.7, -3);
-	glVertex3f(69.2, 44.7, -3);
-	glVertex3f(69, 43, -3);
+	glColor3f(1.0, 0.894, 0.859);
+	glVertex3f(66.8, 43, -3.0);
+	glVertex3f(66.6, 44.7, -3.0);
+	glVertex3f(69.2, 44.7, -3.0);
+	glVertex3f(69, 43, -3.0);
 	glEnd();
 	
 	for(float i=0; i<5; i+=1.4) {
@@ -2084,9 +2133,9 @@ void EffielTower(){
 	
 	glBegin(GL_LINES);
 	glColor3f(0.6, 0.6, 0.6);
-	glVertex3f(67.9, 35.5, -3);
+	glVertex3f(67.9, 35.5, -4);
 	glColor3f(0.9, 0.9, 0.9);
-	glVertex3f(67.9, 58.8, -3);
+	glVertex3f(67.9, 58.8, -4);
 	glEnd();
 	
 	glBegin(GL_POLYGON);
@@ -2114,4 +2163,96 @@ void EffielTower(){
 		glVertex3f(67.6, 52+i, -4);
 		glEnd();
 	}
+}
+
+void Stars1() {
+	
+	//STARS
+	
+	glColor3f(glow1, glow1, glow1);						//Star 1
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(8, 85, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.3, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow1, glow1, glow1);							//Star 2
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(30, 65, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.4, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow1, glow1, glow1);							//Star 3
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(50, 95, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.3, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow1, glow1, glow1);						//Star 4
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(70, 75, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.4, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow1, glow1, glow1);						//Star 5
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(90, 88, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.3, 40, 40);
+	glPopMatrix();
+}
+
+void Stars2() {
+	
+	//STARS
+	
+	glColor3f(glow2, glow2, glow2);						//Star 1.5
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(18, 90, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.3, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow2, glow2, glow2);							//Star 2.5
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(40, 75, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.2, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow2, glow2, glow2);							//Star 3.5
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(55, 78, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.3, 40, 40);
+	glPopMatrix();
+	
+	glColor3f(glow2, glow2, glow2);							//Star 4.5
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glPushMatrix();
+	glTranslatef(80, 65, -9.0);
+	glScalef(0.6, 1.0, 1.0);
+	glutSolidSphere(0.4, 40, 40);
+	glPopMatrix();
+	
 }
